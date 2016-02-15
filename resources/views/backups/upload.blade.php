6dc6f57624c6cdc6e4be98e7f7e2d78cd8d4dab1
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-02-15 15:57:51 +0800

    fix login module name

--- resources/views/backups/upload.blade.php
+++ resources/views/backups/upload.blade.php
@@ -41,7 +41,7 @@
       <div class="col-sm-7 col-md-6 col-centered">
         <div id="panel-tasks" class="panel panel-success">
           <div class="panel-heading">
-            <h3 class="panel-title"><span class="glyphicon glyphicon-cloud-upload"></span> Upload Backup</h3>
+            <h3 class="panel-title"><span class="glyphicon glyphicon-cloud-upload"></span> DropBox</h3>
           </div>
           <div class="panel-body">
             {!! Form::open(['method'=>'PUT', 'url'=>'upload/postfile', 'class'=>'form-horizontal', 'enctype'=>'multipart/form-data']) !!}
