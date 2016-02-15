a86d83312971224632ccfedbe6d0d5d54e689afb
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-02-15 15:55:57 +0800

    fix login module name

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> 
   <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-  <title>GI Cashier - Login('title')</title>
+  <title>GI Cashier - Login</title>
 
   <link rel="shortcut icon" type="image/x-icon" href="/images/g.png" /> 
   <link rel="stylesheet" href="/css/styles-all.min.css">
