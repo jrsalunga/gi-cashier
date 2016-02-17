420aa80e3f3f8cc1d5134d9525a523a4013b3947
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-02-17 10:50:58 +0800

    fix CSRF

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,16 @@ class Handler extends ExceptionHandler
             $e = new NotFoundHttpException($e->getMessage(), $e);
         }
 
+        if(app()->environment()=='production'){
+          if($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+            return redirect('/');
+          }
+        }
+
+        if ($e instanceof \Illuminate\Session\TokenMismatchException) {
+            return redirect('logout');
+        }
+
         return parent::render($request, $e);
     }
 }
