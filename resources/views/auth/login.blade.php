49546df0aec7ed0d60feeb3ac97f60a60b0e2440
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-03-16 14:39:14 +0800

    fix login cache

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> 
   <meta name="csrf-token" content="{{ csrf_token() }}" />
+  <meta http-equiv="Cache-Control" content="no-cache"/>
+  <meta http-equiv="Pragma" content="no-cache">
 
   <title>GI Cashier - Login</title>
 
