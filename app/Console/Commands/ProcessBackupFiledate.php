8db157e080ccbc99bb78bfb7f5f8cbbab396d8b5
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-06-10 13:27:57 +0800

    add filedate parsing

---
+++ app/Console/Commands/ProcessBackupFiledate.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Repositories\BackupRepository;
+
+class ProcessBackupFiledate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backup:filedate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Process backup filedate';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+
+    protected $backup;
+
+    public function __construct(BackupRepository $backuprepository)
+    {
+        parent::__construct();
+
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        
+      $backups = \App\Models\Backup::all();
+
+      foreach ($backups as $backup) {
+
+        $date = $this->backupParseDate($backup->filename);
+        
+        if($date) {
+
+
+          $this->comment('parsing: '. $backup->filename);
+
+          $backup->filedate = $date->format('Y-m-d').' '.$backup->uploaddate->format('H:m:i');
+          
+          if($backup->save()) 
+            $this->comment('saved!');
+          else 
+            $this->comment('not saved!');
+
+
+
+        }
+
+
+      }
+
+
+        
+    }
+
+
+    private function backupParseDate($filename) {
+
+      $f = pathinfo($filename, PATHINFO_FILENAME);
+
+      $m = substr($f, 2, 2);
+      $d = substr($f, 4, 2);
+      $y = '20'.substr($f, 6, 2);
+      
+      if(is_iso_date($y.'-'.$m.'-'.$d))
+        return carbonCheckorNow($y.'-'.$m.'-'.$d);
+      else 
+        return false;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         \App\Console\Commands\Inspire::class,
         \App\Console\Commands\ProcessBackup::class,
+        \App\Console\Commands\ProcessBackupFiledate::class,
         
     ];
 
