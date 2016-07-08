13329fdc6d67a418b7661b40bba8771f1a40d559
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-07-08 17:54:34 +0800

    fix notif

--- app/Events/UserLoggedIn.php
+++ app/Events/UserLoggedIn.php
@@ -18,9 +18,12 @@ class UserLoggedIn extends Event implements ShouldBroadcast
    */
   public function __construct(Request $request)
   {
+    $browser = getBrowserInfo();
     $this->request = $request->all();
     array_set($this->request, 'name', $request->user()->name);
     array_set($this->request, 'ip', clientIP());
+    array_set($this->request, 'browser', $browser['browser']);
+    array_set($this->request, 'platform', $browser['platform']);
   }
 
   /**
@@ -37,7 +40,8 @@ class UserLoggedIn extends Event implements ShouldBroadcast
   {
     return [
       'title'=>'Cashier\'s Module', 
-      'message'=> $this->request['name'].' successfully logged in at this IP address: '. $this->request['ip']
+      'message'=> $this->request['name'].' successfully logged in at '
+      .$this->request['ip'].' using '.$this->request['browser'].' on '. $this->request['platform']
     ];
   }
 }
