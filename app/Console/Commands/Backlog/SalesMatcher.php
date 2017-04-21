77fc239e23288a5608b59cc8177d0d86c2206a00
Jefferson Salunga <jefferson.salunga@gmail.com>
2017-04-21 15:56:10 +0800

    fix

--- app/Console/Commands/Backlog/SalesMatcher.php
+++ app/Console/Commands/Backlog/SalesMatcher.php
@@ -1,9 +1,9 @@
 <?php namespace App\Console\Commands\Backlog;
 
 use DB;
-use App\Helpers\Process;
+use App\Models\Process;
 use Illuminate\Console\Command;
-
+use Carbon\Carbon;
 
 class SalesMatcher extends Command
 {
@@ -64,8 +64,34 @@ class SalesMatcher extends Command
     
     $this->info(' '.count($rs).' ');
 
-    foreach ($rs as $key => $ds) 
+    foreach ($rs as $key => $ds) {
       $this->info(' '.($key+1).' '.$ds->code.' '.$ds->date.' '.$ds->chrg_total.' '.$ds->sales);
+
+
+
+      $proc = Process::where('filedate', $ds->date)->where('code', $ds->code)->first();
+      
+      if (is_null($proc)) {
+
+        $d = Carbon::parse($ds->date);
+        $file = 'GC'.$d->copy()->addDay()->format('mdy').'.ZIP';
+        
+        $attrs = [
+          'filename'  => $file,
+          'filedate'  => $ds->date,
+          'code'      => $ds->code,
+          'path'      => DS.$ds->code.DS.$d->format('Y').DS.$d->format('m').DS.$file,
+          'processed' => 0
+        ];
+
+        $n = Process::create($attrs);
+
+        is_null($n) ? $this->info(' not saved! ') : $this->info(' saved! ');
+      } else {
+        $this->info(' has record on for_process');
+      } 
+
+    }
     
 
 
