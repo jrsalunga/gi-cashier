f1b701d23b6b74719cc0ba2cb741cdd106921956
Jefferson Salunga <jefferson.salunga@gmail.com>
2017-08-15 16:49:40 +0800

    fix

--- app/Listeners/ApEventListener.php
+++ app/Listeners/ApEventListener.php
@@ -18,7 +18,8 @@ class ApEventListener
 
     $data = [
       'subject' => $event->branch->code.' AP '. $event->fileUpload->filename,
-      'to'      => app()->environment()=='production' ? $event->branch->email : 'giligans.app@gmail.com',
+      'to'      => app()->environment()=='production' ? $event->branch->email : 'gi.efiles@gmail.com',
+      'cc'      => 'giligans.app@gmail.com',
       'btn'     => '/AP/'.$event->fileUpload->uploaddate->format('Y/m'),
       'link'    => '/'.strtolower($event->branch->code).'/ap/'.$event->fileUpload->uploaddate->format('Y/m/d').'?src=email',
       'body'    => $event->branch->code.' AP '. $event->fileUpload->filename .' has been uploaded on Cashiers Module'
@@ -30,9 +31,10 @@ class ApEventListener
         $message->subject($data['subject']);
         $message->from('bot@server.loc', 'GI Alerts');
         $message->to($data['to']);
+        $message->cc($data['cc']);
         $message->replyTo('gi.afd01@gmail.com', 'Giligans Accounting');
       });
-      
+
     } catch (\Exception $e) {
 
     }
