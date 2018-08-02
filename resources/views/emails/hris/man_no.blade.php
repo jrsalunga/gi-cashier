dea2001187bf8022fc35ca2c96b1948b940e2c0e
Jefferson Salunga <jefferson.salunga@gmail.com>
2018-08-02 18:37:45 +0800

    fix

---
+++ resources/views/emails/hris/man_no.blade.php
@@ -0,0 +1,9 @@
+<!-- note this template must be on gi-cashier folder -->
+
+<h4>{{ $remarks }}</h4>
+
+<p>Download and save to <a href="file:///C:/GI_PAY">GI_PAY</a> folder</p>
+
+<p>{{ $user }}: {{ $cashier }}</p>
+
+<small><em>Note: this is a system generated email.</em></small>
