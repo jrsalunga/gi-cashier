fecfbd9f1c78895d2cbe42109976f5128e933e07
Jefferson Salunga <jefferson.salunga@gmail.com>
2018-09-05 16:29:10 +0800

    fix

--- app/Repositories/SetslpRepository.php
+++ app/Repositories/SetslpRepository.php
@@ -61,12 +61,17 @@ class SetslpRepository extends BaseRepository implements CacheableInterface
       $arr[$i]['date'] = $d;
       $arr[$i]['total'] = 0;
     
+      //$data = $this->aggregateDailyLogs(c($d->copy()->subDay()->format('Y-m-d').' 10:00:00'), c($d->format('Y-m-d').' 07:59:59'));
+
       $filtered = $data->filter(function ($item) use ($d){
         $s = c($d->format('Y-m-d').' 10:00:00');
         $e = c($d->copy()->addDay()->format('Y-m-d').' 09:59:59');
         $i = c($item->date->format('Y-m-d').' '.$item->time);
 
-        return $i->gte($s) && $i->lte($e)
+        $t = $item->date->gte($s) && $item->date->lte($e) ? $item->amount:'F';
+        
+        //test_log($s.' < '.$i.' > '.$e.' = '.$t);
+        return $item->date->gte($s) && $item->date->lte($e)
           ? $item : null;
       });
 
