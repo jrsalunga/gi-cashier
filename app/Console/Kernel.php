f37233be9be15cfdeace171e2507108cdfd0f3e7
Jefferson Salunga <jefferson.salunga@gmail.com>
2019-07-17 18:52:26 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -43,10 +43,10 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\Import\BacklogCharges::class,
         \App\Console\Commands\Cron\BacklogCharges::class,
 
-        \App\Console\Commands\Backlog\MonthlyChangeItem::class
+        \App\Console\Commands\Backlog\MonthlyChangeItem::class,
         \App\Console\Commands\Import\ChangeItem::class,
         \App\Console\Commands\Cron\ChangeItem::class,
-        ,
+        
     ];
 
     /**
