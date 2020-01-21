ad790940bdc6cb86a4d529e232deb1b02680d89e
Jefferson Salunga <jefferson.salunga@yahoo.com>
2020-01-21 18:19:17 +0800

    fix

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -191,8 +191,8 @@ class AuthController extends Controller
         Auth::login($au, true);
 
         if (app()->environment()==='production')
-            event(new GoogleUserLoggedIn($user->email, substr($user->getAvatar(),0,-5)));
+            event(new GoogleUserLoggedIn($user->email, substr($user->getAvatar(),0,-4)));
         
-        return redirect('/?rdr=google&avatar='.$user->getAvatar())->withCookie(cookie('avatar',  substr($user->getAvatar(),0,-5), 45000));
+        return redirect('/?rdr=google&avatar='.substr($user->getAvatar(),0,-4))->withCookie(cookie('avatar',  substr($user->getAvatar(),0,-4), 45000));
     }
 }
