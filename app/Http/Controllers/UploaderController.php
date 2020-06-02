df001bd1608c6a4dd74b0ad9928448358ee4361b
Jefferson Salunga <jefferson.salunga@yahoo.com>
2020-06-02 20:55:22 +0800

    fix

--- app/Http/Controllers/UploaderController.php
+++ app/Http/Controllers/UploaderController.php
@@ -1130,7 +1130,7 @@ class UploaderController extends Controller
 
     try {
 
-      $m = \Mail::queue('emails.notifier', $data, function ($message) {
+      $m = \Mail::send('emails.notifier', $data, function ($message) {
         $message->subject('Test Emailss');
         $message->from('test@gmail.com');
         $message->to('jefferson@gmail.com');
