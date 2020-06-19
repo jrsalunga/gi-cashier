6028bbc77cf344f54899740171d227703598a1a0
Jefferson Salunga <jefferson.salunga@yahoo.com>
2020-06-19 11:59:15 +0800

    fix

--- app/Repositories/ApUploadRepository.php
+++ app/Repositories/ApUploadRepository.php
@@ -5,12 +5,17 @@ use Prettus\Repository\Eloquent\BaseRepository;
 use Prettus\Repository\Traits\CacheableRepository;
 use Prettus\Repository\Contracts\CacheableInterface;
 use App\Traits\Repository as RepoTrait;
+use App\Repositories\Criterias\ByBranch2;
 
 class ApUploadRepository extends BaseRepository implements CacheableInterface
 {
   use CacheableRepository, RepoTrait;
 
-  protected $order = ['descriptor'];
+ protected $order = ['created_at'];
+
+  public function boot(){
+    $this->pushCriteria(new ByBranch2(request()));
+  }
   
   public function model() {
     return 'App\\Models\\ApUpload';
