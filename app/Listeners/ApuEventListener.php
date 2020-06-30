61959349f85372f008deee746c53677a3ded8915
Jefferson Salunga <jefferson.salunga@yahoo.com>
2020-06-30 17:58:32 +0800

    fix

--- app/Listeners/ApuEventListener.php
+++ app/Listeners/ApuEventListener.php
@@ -30,7 +30,7 @@ class ApuEventListener
     if (app()->environment('production')) {
       $l = 'http://am.giligansrestaurant.com/ap/';
       $c = 'http://cashier.giligansrestaurant.com/'.strtolower($brcode).'/apu/';
-      $e['mailing_list'] = $this->bossBranch->getUsers();
+      $e['mailing_list'] = $this->bossBranch->getUsers()->toArray();
       // $e['mailing_list'] = [
       //   ['name'=>'Jefferson Salunga', 'email'=>'jefferson.salunga@gmail.com'],
       //   ['name'=>'Jeff Salunga', 'email'=>'freakyash02@gmail.com'],
@@ -58,14 +58,14 @@ class ApuEventListener
     // 1. notify the cashier
     $e['to'] = $email_csh;
     $e['link'] =  $c.$event->model->lid();
-    $this->mailer->queue('docu.apu.mail-upload', $e, function ($message) use ($e) {
-      $message->subject($e['subject']);
-      $message->from('giligans.app@gmail.com', 'GI Payables');
-      $message->to($e['to']);
+    // $this->mailer->queue('docu.apu.mail-upload', $e, function ($message) use ($e) {
+    //   $message->subject($e['subject']);
+    //   $message->from('giligans.app@gmail.com', 'GI Payables');
+    //   $message->to($e['to']);
 
-      // if (!is_null($e['attachment']))
-      //   $message->attach($e['attachment']);
-    });
+    //   // if (!is_null($e['attachment']))
+    //   //   $message->attach($e['attachment']);
+    // });
 
     // 2. RM, AHC 
     $e['to'] = $event->model->type==2 ? env('AP_K_EMAIL') : env('AP_C_EMAIL');
@@ -77,7 +77,7 @@ class ApuEventListener
       $message->replyTo($e['replyTo']);
 
        foreach ($e['mailing_list'] as $u)
-        $message->to($e['email'], $e['name']);
+        $message->to($u['email'], $u['name']);
 
       // if (!is_null($e['attachment']))
         // $message->attach($e['attachment']);
