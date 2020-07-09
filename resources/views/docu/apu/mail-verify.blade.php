fdd1a4d88ecb4f17d484074748c94cf60abac2ae
Jefferson Salunga <jefferson.salunga@yahoo.com>
2020-07-09 12:45:28 +0800

    fix

--- resources/views/docu/apu/mail-verify.blade.php
+++ resources/views/docu/apu/mail-verify.blade.php
@@ -14,7 +14,9 @@
 <p>- {{ $model->cashier }}</p>
 <p>&nbsp;</p>
 
+@if(is_null($$model->terms=='K'))
 <p>To be able to process the Cheque of this transaction, kindly verify the encoded information and the attached document. If it matches, click the <b style="color:#449D44;">Mark as Verified</b>, if not, kindly delete the AP upload and instruct the cashier to re-upload the correct document.</p>
+@endif
 
 <p>
   <a href="{{ $link }}?src=email" style="display: inline-block;
