1e102ff0dc191ee3708686a028d72c2b6742cb55
Jefferson Salunga <jefferson.salunga@yahoo.com>
2020-07-10 14:03:58 +0800

    fix Google avatar

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -211,6 +211,6 @@ class AuthController extends Controller
         if (app()->environment()==='production')
             event(new GoogleUserLoggedIn($user->email, substr($user->getAvatar(),0,-4)));
         
-        return redirect('/?rdr=google&avatar='.substr($user->getAvatar(),0,-4))->withCookie(cookie('avatar',  substr($user->getAvatar(),0,-4), 45000));
+        return redirect('/?rdr=google&avatar='.$user->getAvatar())->withCookie(cookie('avatar',  substr($user->getAvatar(),0,-4), 45000));
     }
 }
