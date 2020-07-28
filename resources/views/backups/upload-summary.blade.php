ebd9b9a31990b78d8b550b2e5caef1d72a8d362b
Jefferson Salunga <jefferson.salunga@yahoo.com>
2020-07-28 17:29:51 +0800

    fix

--- resources/views/backups/upload-summary.blade.php
+++ resources/views/backups/upload-summary.blade.php
@@ -196,18 +196,18 @@
                     <td class="text-right">{{ nf($cash_audit->col_dink, 2, true) }}</td>
                   </tr>
                   <tr>
-                    <td>&nbsp;</td>
-                    <td class="text-right">{{ nf($cash_audit->cas+$cash_audit->ca, 2, true) }}</td>
-                    <td class="text-right">{{ nf($cash_audit->cak, 2, true) }}</td>
+                    <td>{{ $cash_audit->col_cas }}</td>
+                    <td class="text-right">{{ nf($cash_audit->col_ca, 2, true) }}</td>
+                    <td class="text-right">{{ nf($cash_audit->col_cak, 2, true) }}</td>
                   </tr>
                   <tr>
-                    <td>&nbsp;</td>
-                    <td class="text-right">{{ nf($cash_audit->col_oths+$cash_audit->col_othr, 2, true) }}</td>
+                    <td>{{ $cash_audit->col_oths }}</td>
+                    <td class="text-right">{{ nf($cash_audit->col_othr, 2, true) }}</td>
                     <td class="text-right">{{ nf($cash_audit->col_othrk, 2, true) }}</td>
                   </tr>
                   <tr>
-                    <td>&nbsp;</td>
-                    <td class="text-right">{{ nf($cash_audit->coloth2s+$cash_audit->col_oth2, 2, true) }}</td>
+                    <td>{{ $cash_audit->coloth2s }}</td>
+                    <td class="text-right">{{ nf($cash_audit->col_oth2, 2, true) }}</td>
                     <td class="text-right">{{ nf($cash_audit->col_oth2k, 2, true) }}</td>
                   </tr>
                   <tr>
