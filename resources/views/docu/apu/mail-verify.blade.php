df65c70de50149378953a08aa497c5dd134e8f7a
Jefferson Salunga <jefferson.salunga@yahoo.com>
2020-09-28 11:18:10 +0800

    fix

--- resources/views/docu/apu/mail-verify.blade.php
+++ resources/views/docu/apu/mail-verify.blade.php
@@ -13,10 +13,8 @@
 	<li title="Transmittal Date">{{ $model->date->format('D M j, Y') }}</li>
 	<li title="Amount">{{ number_format($model->amount, 2) }}</li>
 </ul>
-
-<p>{{ $model->remarks }}</p>
 <p>- {{ $model->cashier }}</p>
-<p>&nbsp;</p>
+<p>{{ $model->remarks }}</p>
 
 @if($model->type=='2')
 <p>To be able to process the Cheque of this transaction, kindly verify the encoded information and the attached document. If it matches, click the <b style="color:#449D44;">Mark as Verified</b> on the <b>Area Manager Module</b>, if not, kindly delete the AP upload and instruct the cashier to re-upload the correct document.</p>
@@ -48,8 +46,6 @@
     Verify Document
   </a>
 </p>
-
-<p>&nbsp;</p>
 <small><em>Note: this is a system generated email.</em></small>
 
 
