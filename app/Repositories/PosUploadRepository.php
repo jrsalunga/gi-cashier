d14e35385999eb0bbb6daf0872a750e4569bb34f
Jefferson Salunga <jefferson.salunga@yahoo.com>
2021-09-20 13:58:16 +0800

    fix

--- app/Repositories/PosUploadRepository.php
+++ app/Repositories/PosUploadRepository.php
@@ -1616,7 +1616,7 @@ class PosUploadRepository extends Repository
             }
           }
 
-          if (starts_with(strtolower($data['tblno']), 'z'))
+          if (starts_with(strtolower($data['tblno']), 'z') && $data['chrg_type']=='ZAP')
             $ds['zap_sales'] += $data['tot_chrg'];
 
         }
@@ -1726,7 +1726,7 @@ class PosUploadRepository extends Repository
             }
           }
 
-          if (starts_with(strtolower($data['tblno']), 'z'))
+          if (starts_with(strtolower($data['tblno']), 'z') && $data['chrg_type']=='ZAP')
             $ds['zap_sales'] += $data['tot_chrg'];
 
         }
@@ -2781,7 +2781,7 @@ class PosUploadRepository extends Repository
             }
           }
 
-          if (starts_with(strtolower($data['tblno']), 'z'))
+          if (starts_with(strtolower($data['tblno']), 'z') && $data['chrg_type']=='ZAP')
             $ds['zap_sales'] += $data['tot_chrg'];
 
         }
@@ -2892,7 +2892,7 @@ class PosUploadRepository extends Repository
             }
           }
 
-          if (starts_with(strtolower($data['tblno']), 'z'))
+          if (starts_with(strtolower($data['tblno']), 'z') && $data['chrg_type']=='ZAP')
             $ds['zap_sales'] += $data['tot_chrg'];
 
         }
