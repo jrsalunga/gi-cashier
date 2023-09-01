e44710fc1b6ba8113f61370c44b8b4fa1fbd50c8
Jefferson Salunga <jefferson.salunga@yahoo.com>
2023-09-01 16:32:06 +0800

    fix

--- app/Listeners/ProcessesEventListener.php
+++ app/Listeners/ProcessesEventListener.php
@@ -67,51 +67,27 @@ class ProcessesEventListener
 
     foreach ($month as $key => $value) {
 
-      if (isset($value->component)) {
+      $ord = isset($value->component->compcat->expense->ordinal)
+      ? $value->component->compcat->expense->ordinal
+      : 833;
 
-        $ord = isset($value->component->compcat->expense->ordinal)
-        ? $value->component->compcat->expense->ordinal
-        : 833;
+      //$ord = 883;
 
-        $this->mc->firstOrNewField([
-          'date'          => $event->date->copy()->lastOfMonth()->format('Y-m-d'),
-          'component_id'  => $value->componentid,
-          'uom'           => $value->uom,
-          'expensecode'   => $value->component->compcat->expense->code,
-          'expense_id'    => $value->component->compcat->expenseid,
-          'qty'           => $value->qty,
-          'tcost'         => $value->tcost,
-          'trans'         => $value->trans,
-          'branch_id'     => $event->branchid,
-          'status'        => $value->status,
-          'ordinal'       => $ord,
-        ], ['date', 'branch_id', 'component_id']);
-        # code...
-   
-      } 
-
-
-      
-
-      // $ord = isset($value->component->compcat->expense->ordinal)
-      // ? $value->component->compcat->expense->ordinal
-      // : 833;
-
-      // //$ord = 883;
-
-      // $this->mc->firstOrNewField([
-      //   'date'          => $event->date->copy()->lastOfMonth()->format('Y-m-d'),
-      //   'component_id'  => $value->componentid,
-      //   'uom'           => $value->uom,
-      //   'expensecode'   => $value->component->compcat->expense->code,
-      //   'expense_id'    => $value->component->compcat->expenseid,
-      //   'qty'           => $value->qty,
-      //   'tcost'         => $value->tcost,
-      //   'trans'         => $value->trans,
-      //   'branch_id'     => $event->branchid,
-      //   'status'        => $value->status,
-      //   'ordinal'       => $ord,
-      // ], ['date', 'branch_id', 'component_id']);
+      $this->mc->firstOrNewField([
+        'date'          => $event->date->copy()->lastOfMonth()->format('Y-m-d'),
+        'component_id'  => $value->componentid,
+        'uom'           => $value->uom,
+        // 'expensecode'   => 'xxxx',
+        // 'expense_id'    => 'yyyy',
+        'expensecode'   => $value->component->compcat->expense->code,
+        'expense_id'    => $value->component->compcat->expenseid,
+        'qty'           => $value->qty,
+        'tcost'         => $value->tcost,
+        'trans'         => $value->trans,
+        'branch_id'     => $event->branchid,
+        'status'        => $value->status,
+        'ordinal'       => $ord,
+      ], ['date', 'branch_id', 'component_id']);
       # code...
     }
 
