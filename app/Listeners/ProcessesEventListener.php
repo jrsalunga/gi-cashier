a8ece4b4f9b540397e4b95ecfcc69b8d55b6c5cb
Jefferson Salunga <jefferson.salunga@yahoo.com>
2023-09-01 16:20:53 +0800

    fix

--- app/Listeners/ProcessesEventListener.php
+++ app/Listeners/ProcessesEventListener.php
@@ -76,25 +76,25 @@ class ProcessesEventListener
 
       exit;
 
-      $ord = isset($value->component->compcat->expense->ordinal)
-      ? $value->component->compcat->expense->ordinal
-      : 833;
+      // $ord = isset($value->component->compcat->expense->ordinal)
+      // ? $value->component->compcat->expense->ordinal
+      // : 833;
 
-      //$ord = 883;
+      // //$ord = 883;
 
-      $this->mc->firstOrNewField([
-        'date'          => $event->date->copy()->lastOfMonth()->format('Y-m-d'),
-        'component_id'  => $value->componentid,
-        'uom'           => $value->uom,
-        'expensecode'   => $value->component->compcat->expense->code,
-        'expense_id'    => $value->component->compcat->expenseid,
-        'qty'           => $value->qty,
-        'tcost'         => $value->tcost,
-        'trans'         => $value->trans,
-        'branch_id'     => $event->branchid,
-        'status'        => $value->status,
-        'ordinal'       => $ord,
-      ], ['date', 'branch_id', 'component_id']);
+      // $this->mc->firstOrNewField([
+      //   'date'          => $event->date->copy()->lastOfMonth()->format('Y-m-d'),
+      //   'component_id'  => $value->componentid,
+      //   'uom'           => $value->uom,
+      //   'expensecode'   => $value->component->compcat->expense->code,
+      //   'expense_id'    => $value->component->compcat->expenseid,
+      //   'qty'           => $value->qty,
+      //   'tcost'         => $value->tcost,
+      //   'trans'         => $value->trans,
+      //   'branch_id'     => $event->branchid,
+      //   'status'        => $value->status,
+      //   'ordinal'       => $ord,
+      // ], ['date', 'branch_id', 'component_id']);
       # code...
     }
 
