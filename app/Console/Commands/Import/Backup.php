5783526a174864c4690ff796fbee8e8cb6b7b92e
Jefferson Salunga <jefferson.salunga@yahoo.com>
2024-02-29 16:42:02 +0800

    fix

---
+++ app/Console/Commands/Import/Backup.php
@@ -0,0 +1,153 @@
+<?php namespace App\Console\Commands\Import;
+
+use DB;
+use Carbon\Carbon;
+use App\Models\Branch;
+use App\Helpers\Locator;
+use Illuminate\Console\Command;
+use App\Repositories\PosUploadRepository as PosUploadRepo;
+use App\Models\Process;
+use App\Repositories\DateRange;
+use App\Events\Notifier;
+
+class Backup extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'import:backup {--reset= : reset}';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Import backlog month of a branch on CRON';
+
+  protected $process;
+
+  protected $dr;
+
+  protected $type = 11;
+
+  public function __construct(Process $process, DateRange $dr)
+  {
+    parent::__construct();
+    $this->process = $process;
+    $this->dr = $dr;
+  }
+  
+
+
+
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function handle() {
+
+    $locator = new Locator('pos');
+    // $locator->allFiles();
+
+    $fs = $locator->allFiles('TEST_POS_BACKUP');
+    $td = NULL;
+
+    foreach($fs as $k => $v) {
+      
+      $f = explode('\\',$v);
+      $d = filename_to_date2($f[8]);
+      // $this->info($d->format('Y-m-d'));
+        
+      if (is_null($td))
+        $td=$d->copy()->lastOfMonth();
+
+      // if ($td->format('m')==$d->format('m')) {
+      if (($td->format('m')==$d->format('m')) && ($td->format('Y')==$d->format('Y'))) {
+        $this->info($k.' L1 '.$f[5].' '.$td->format('Y-m-d').'=='.$d->format('Y-m-d'));
+
+        // 31      31 
+        $temp = $d;
+      } else {
+      
+        // $this->info($k.' '.$td->format('Y-m-d').'=='.$temp->format('Y-m-d').'  temp');
+        if ($td->eq($temp))
+          $this->info('is eom! '. $temp->format('Y-m-d'));
+        else
+          $this->info('not eom! '. $temp->format('Y-m-d'));
+
+  
+        // $this->info($k.' '.$td->format('Y-m-d').'=='.$d->format('Y-m-d'));
+        $td=$d->copy()->lastOfMonth();
+        $temp = $d;
+        $this->info($k.' L2 '.$f[5].' '.$td->format('Y-m-d').'=='.$d->format('Y-m-d'));
+      }
+
+      // $this->info($k.' '.$f[5].'  '.$f[6].'  '.$f[7].'  '.$td.'  '.$f[8]);
+    }
+
+
+
+    exit;
+   
+    
+
+    $reset = is_null($this->option('reset')) 
+      ? false
+      : $this->option('reset')=='true' ? true:false;
+
+    $this->info('reset: '.$reset);
+
+    $this->dr->fr = $from;
+    $this->dr->to = $to;
+
+    foreach ($this->dr->monthInterval() as $key => $date) {
+
+      $eom = $date->copy()->endOfMonth();
+      $this->line($eom->format('Y-m-d'));
+      
+      $locator = new Locator('pos');
+      $backup = 'GC'.$eom->format('mdy').'.ZIP';
+      $path = $br->code.DS.$eom->format('Y').DS.$eom->format('m').DS.$backup;
+      
+      //$this->info($locator->realFullPath($path));
+      if ($locator->exists($path)) {
+
+        $attr = [
+          'filename'  => $backup,
+          'filedate'  => $eom->format('Y-m-d'),
+          'code'      => $br->code,
+          'path'      => $path,
+          'type'      => $this->type
+        ];
+        
+        $p = $this->process->firstOrCreate($attr);
+        
+        if ($p) {
+          $this->line('loaded');
+          if ($reset) {
+            $p->processed = 0; 
+            $p->save();
+          }
+        } else
+          $this->error('error on inserting processed');
+      } else {
+        $this->error('backup do not exist');
+      }
+      
+    }
+
+    
+    
+
+
+    
+    exit;
+  }
+
+
+
+   
+}
